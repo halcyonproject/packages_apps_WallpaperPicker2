6529b8ed788b4b9f632f7fcf2f73249fc007fae7
Satish Yalla <satishy@google.com>
2025-06-29 19:56:12 -0700

    Revert "Select both home and lock screen by default in Apply Wallpaper screen"
    This reverts commit f10c45f4bd3943cb2e00fa97d6b143f835ed2182.
    
    Reason for revert: Droidmonitor created revert due to b/428329798. Will be verifying through ABTD before submission
    
    Bug: 428329798
    Change-Id: I9b73c8ec510bf69cdaa8e96d479701cf0e704527

--- src/com/android/wallpaper/picker/preview/ui/viewmodel/WallpaperPreviewViewModel.kt
+++ src/com/android/wallpaper/picker/preview/ui/viewmodel/WallpaperPreviewViewModel.kt
@@ -497,7 +497,7 @@ constructor(
             (enabledSet, dest) ->
             if (enabledSet) {
                 {
-                    val screens = dest?.toScreens() ?: WallpaperDestination.BOTH.toScreens()
+                    val screens = dest?.toScreens() ?: setOf(smallPreviewSelectedTab.value)
                     initSetWallpaperDialogScreenSelected(screens)
                     setApplyWallpaperPreviewSelectedTab(smallPreviewSelectedTab.value)
                     _currentPreviewScreen.value = PreviewScreen.APPLY_WALLPAPER
